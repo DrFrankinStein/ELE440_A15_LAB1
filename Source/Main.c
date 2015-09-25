adcc4e2a4077b89ba00505f4d3deb6ef87d54d4c
DrFrankinStein <julienlemay2@gmail.com>
2015-09-25 14:56:00 -0400

    Algorithme principal de test
    Développement du test principal avec lecture du temps d'exécution, du
    nombre d'instruc.

--- Source/Main.c
+++ Source/Main.c
@@ -7,16 +7,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "Testing.h"
-
+#include <time.h>
+#include <windows.h>
 /*
  * 
  */
+//https://msdn.microsoft.com/en-us/library/windows/desktop/ms644905(v=vs.85).aspx
+//http://www.songho.ca/misc/timer/timer.html
 int main(int argc, char** argv)
 {
-/*
+
     //Déclaration des variables
-    
+   
     int iSucces;
     int i; //itérateur pour nombre de données
     int j; //itérateur pour taille des nombres
@@ -27,7 +31,14 @@ int main(int argc, char** argv)
     int m;
     int N[NSIZE];
     
+    
+    LARGE_INTEGER start, end, average, frequency;
+    unsigned long long barometre, avrgBarometre;
+    char nomTest[50] = "TriBase";
+    
     //Initialisation des variables
+    
+    QueryPerformanceFrequency(&frequency);
     
     //Initialisation de la matrice N
 
@@ -36,31 +47,49 @@ int main(int argc, char** argv)
         N[i] = NFACTOR * K[i];
     }
     
+    writeDataFileInit(nomTest);
+    
     for (i = 1; i <= NSIZE; i++)
     {
-        n = N[i];
+        n = N[i-1];
         int T1[n];
+        printf("Nombre de donnees = %i (%i/%i)\n\r",n,i,NSIZE);
         for(j = 1; j <= RSIZE; j++)
         {
-            r = R[j];
+            r = R[j-1];
+            printf("  Intervalle de donnees = %i (%i/%i)\n\r    Taux de desordre=\n\r",r,j,RSIZE);
             for(k = 1; k <= DSIZE; k++)
             {
-                d = D[k];
+                d = D[k-1];
+                printf("      (%i/%i) [%i%%] : ",k,DSIZE,d);
+                average.QuadPart = 0;
+                avrgBarometre = 0;
                 for (m = 1; m <= 10; m++)
                 {
-                    T1[] = GenererDonnees(n, r, d);
-                    TriParInsertion(T1, n, r);
-                    TriPigeonnier(T1, n, r);
-                    TriRapide(T1, n, r);
-                    TriParFusion(T1, n, r);
-                    TriParTas(T1, n, r);
-                    TriParBase(T1, n, r);
+                    GenererDonnees(T1, n,r,d);
+                    QueryPerformanceCounter(&start);
+                    
+                    barometre=TriParBase(T1,n,2*j+1);
+                    //barometre=TriParFusion(T1,0,n-1);
+                    //barometre=TriParInsertion(T1,n);
+                    
+                    
+                    QueryPerformanceCounter(&end);
+                    
+                    writeDataFile(nomTest, n, r, d,barometre, (int)((1000000*(end.QuadPart-start.QuadPart))/frequency.QuadPart));
+                    
+                    average.QuadPart+=end.QuadPart-start.QuadPart;
+                    avrgBarometre+=barometre;
+                    //printf("%i ", (int)((1000000*(end.QuadPart-start.QuadPart))/frequency.QuadPart));
+                    //printf("%llu ", barometre);
                 }
+                printf("~%llu instr. ~%i us\n\r",avrgBarometre/10,(int)((average.QuadPart*1000000)/(10*frequency.QuadPart)));
             }
         }
     }
-    */
-    TestJulien();
+    
+    printf("int=%i bits long=%i bits long long=%i bits\n\r",sizeof(int)*8,sizeof(long)*8, sizeof(long long)*8);
+    //TestJulien();
     //TestAlexandre();
     return (EXIT_SUCCESS);
 }
